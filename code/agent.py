fa10c22b1ac7a8adb712d360af94f6843fbda3b3
lokikiller <haotingyi13@otcaix.iscas.ac.cn>
2015-12-07 15:35:03 +0800

    feature completed

--- code/agent.py
+++ code/agent.py
@@ -1,4 +1,7 @@
+import sched
+import sys, time
 import argparse
+from data.getdata import GetData
 
 """
 author: hty / zby
@@ -6,27 +9,38 @@ create: 2015.11.30
 agent.py -- catch avg_load cpu and memory usage
 """
 
-parser = argparse.ArgumentParser(prog='agent.py', usage='python %(prog)s [Options]', add_help=False)
-group = parser.add_argument_group('Options')
-group.add_argument('-v', '--version', action='version', help='show program version number and exit')
-group.add_argument('-h', '--help', action='help', help='show this help message and exit')
-group.add_argument('-t', '--ttl', action='store', type=int, default=60, dest='ttl', metavar='',
-                   help='set agent period, default is 60s')
-group.add_argument('-m', '--module=', dest='MODULE', required=True, action='store',
-                   help='use module %(dest)s')
+scheduler = sched.scheduler(time.time, time.sleep)
 
-groupmodules = parser.add_argument_group('Modules')
-groupmodules.add_argument('all, cpu, memory', action='store_const', const=0)
 
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(prog='agent.py', usage='python %(prog)s [Options]', add_help=False)
+    group = parser.add_argument_group('Options')
+    group.add_argument('-v', '--version', action='version', help='show program version number and exit')
+    group.add_argument('-h', '--help', action='help', help='show this help message and exit')
+    group.add_argument('-t', '--ttl', action='store', type=int, default=60, dest='ttl', metavar='',
+                       help='set agent period, default is 60s')
+    group.add_argument('-m', '--module=', dest='MODULE', required=True, action='store',
+                       help='use module %(dest)s')
 
-ttl = args.ttl
-print ttl
+    groupmodules = parser.add_argument_group('Modules')
+    groupmodules.add_argument('load, cpu, memory', action='store_const', const=0)
 
-module = args.MODULE
-if module == 'all':
-    print module
-elif module == 'cpu':
-    print module
-elif module == 'memory':
-    print module
+    args = parser.parse_args()
+
+    ttl = args.ttl
+    inittime = time.time()
+    module = args.MODULE
+
+    scheduler.enterabs(inittime, 1, catchdata, (module, inittime, ttl,))
+    scheduler.run()
+
+
+def catchdata(mod, action_time, ttl):
+    datas = GetData(mod).catch()
+    for data in datas:
+        print '%-15s\t%-15s' % (data, datas[data])
+    scheduler.enterabs(action_time + ttl, 1, catchdata, (mod, action_time + ttl, ttl,))
+
+
+if __name__ == '__main__':
+    main()
---
+++ code/data/getdata.py
@@ -0,0 +1,42 @@
+import json
+import subprocess
+from exception.module import ModuleException
+
+"""
+author: hty / zby
+create: 2015.12.07
+getdata.py -- catch avg_load cpu and memory usage from scripts
+"""
+
+
+class GetData(object):
+
+
+    def __init__(self, mod):
+        self.__scriptPath = '../scripts/'
+        self.mod = mod
+
+    def __get_cpu(self):
+        return subprocess.check_output(self.__scriptPath + 'cpu.sh')
+
+    def __get_memory(self):
+        return subprocess.check_output(self.__scriptPath + 'mem.sh')
+
+    def __get_load(self):
+        return subprocess.check_output(self.__scriptPath + 'loadavg.sh')
+
+    def catch(self):
+        '''
+        catching data form different module, if module is not in [cpu, memory, load], raise exception
+        :return: data format dict
+        '''
+        data = ''
+        if self.mod == 'cpu':
+            data = self.__get_cpu()
+        elif self.mod == 'memory':
+            data = self.__get_memory()
+        elif self.mod == 'load':
+            data = self.__get_load()
+        else:
+            raise ModuleException('no such module ' + self.mod)
+        return json.loads(data)
