3cdc8aa9c59bb424d21fd36dcebe44b166956399
lokikiller <haotingyi13@otcaix.iscas.ac.cn>
2015-12-15 09:36:22 +0800

    fix bugs and change some function from fei-god

--- code/agent.py
+++ code/agent.py
@@ -1,80 +1,95 @@
+#!/usr/bin/env python
+# -*- encoding=utf8 -*-
+
+'''
+FileName:   agent
+Author:     Hao Tingyi
+@contact:   lokikiller@126.com
+@version:   $
+
+Description:
+    usage: python agent.py [Options]
+
+    Options:
+      -v, --version         show program version number and exit
+      -h, --help            show this help message and exit
+      -t , --ttl            set agent period, default is 60s
+      -m MODULE, --module= MODULE
+                            use module MODULE
+
+    Modules:
+      load, cpu, memory
+Changelog:
+    create at 2015.12.14
+'''
+
 import argparse
 import curses
 import sched
 import time
-from os.path import dirname, join
 
-from data.getdata import GetData
-
-"""
-author: hty / zby
-create: 2015.11.30
-agent.py -- catch avg_load cpu and memory usage
-
-usage: python agent.py [Options]
-
-Options:
-  -v, --version         show program version number and exit
-  -h, --help            show this help message and exit
-  -t , --ttl            set agent period, default is 60s
-  -m MODULE, --module= MODULE
-                        use module MODULE
-
-Modules:
-  load, cpu, memory
-"""
+from data.collector import DataCollection
 
 
-def main():
-    with open(join(dirname(__file__), 'version/VERSION'), 'rb') as f:
-        version = f.read().decode('ascii').strip()
+class CollectorCLI(object):
+    def __init__(self):
+        self.VERSION = '0.1.0'
+        self.__scheduler = sched.scheduler(time.time, time.sleep)
 
-    parser = argparse.ArgumentParser(prog='agent.py', usage='python %(prog)s [Options]', add_help=False)
-    group = parser.add_argument_group('Options')
-    group.add_argument('-v', '--version', action='version', version=version,
-                       help='show program version number and exit')
-    group.add_argument('-h', '--help', action='help', help='show this help message and exit')
-    group.add_argument('-t', '--ttl', action='store', type=int, default=60, dest='ttl', metavar='',
-                       help='set agent period, default is 60s')
-    group.add_argument('-m', '--module=', dest='MODULE', required=True, action='store',
-                       help='use module %(dest)s')
+    def cli(self):
+        parser = argparse.ArgumentParser(prog='agent.py',
+                                         usage='python %(prog)s [Options]',
+                                         add_help=False)
+        group = parser.add_argument_group('Options')
+        group.add_argument('-v', '--version', action='version',
+                           version=self.VERSION,
+                           help='show program version number and exit')
+        group.add_argument('-h', '--help', action='help',
+                           help='show this help message and exit')
+        group.add_argument('-t', '--ttl', action='store', type=int, default=60,
+                           dest='ttl', metavar='',
+                           help='set agent period, default is 60s')
+        group.add_argument('-m', '--module=', dest='MODULE', required=True,
+                           action='store',
+                           help='use module %(dest)s')
 
-    groupmodules = parser.add_argument_group('Modules')
-    groupmodules.add_argument('load, cpu, memory', action='store_const', const=0)
+        groupmodules = parser.add_argument_group('Modules')
+        groupmodules.add_argument('load, cpu, memory', action='store_const',
+                                  const=0)
 
-    args = parser.parse_args()
-    try:
-        screen = curses.initscr()
-        screen.clear()
+        args = parser.parse_args()
+        try:
+            screen = curses.initscr()
+            screen.clear()
 
-        ttl = args.ttl
-        inittime = time.time()
-        module = args.MODULE
+            ttl = args.ttl
+            inittime = time.time()
+            module = args.MODULE
 
-        scheduler.enterabs(inittime, 1, catchdata, (module, inittime, ttl, screen,))
-        scheduler.run()
-    except Exception:
-        curses.endwin()
-        parser.parse_args(['-h'])
+            self.__scheduler.enterabs(inittime, 1, self.__catchdata,
+                                      (module, inittime, ttl, screen,))
+            self.__scheduler.run()
+        except Exception:
+            curses.endwin()
+            parser.parse_args(['-h'])
 
+    # TODO: Add unit name
+    def __catchdata(self, mod, action_time, ttl, screen):
+        datas = DataCollection(mod).catch()
+        i = 0
+        for data in datas:
+            i += 1
+            str = '{:<15}\t{:<25}'.format(data, datas[data])
+            screen.addstr(i, 0, str, curses.A_NORMAL)
+            screen.refresh()
 
-# TODO: Add unit name
-def catchdata(mod, action_time, ttl, screen):
-    datas = GetData(mod).catch()
-    i = 0
-    for data in datas:
-        i += 1
-        str = '{:<15}\t{:<15}'.format(data, datas[data])
-        screen.addstr(i, 0, str, curses.A_NORMAL)
-        screen.refresh()
-
-    scheduler.enterabs(action_time + ttl, 1, catchdata, (mod, action_time + ttl, ttl, screen,))
+        self.__scheduler.enterabs(action_time + ttl, 1, self.__catchdata,
+                                  (mod, action_time + ttl, ttl, screen,))
 
 
 if __name__ == '__main__':
     try:
-        scheduler = sched.scheduler(time.time, time.sleep)
-        main()
+        CollectorCLI().cli()
     except KeyboardInterrupt:
         curses.endwin()
         exit()
---
+++ code/data/collector.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# -*- encoding=utf8 -*-
+
+'''
+FileName:   collector
+Author:     Hao Tingyi
+@contact:   lokikiller@126.com
+@version:   $
+
+Description:
+    collect cpu/memory/load data from /proc file
+Changelog:
+    create at 2015.12.15
+'''
+
+import subprocess
+import time
+import collections
+from exception.module import ModuleException
+
+
+class DataCollection(object):
+    def __init__(self, mod):
+        self.mod = mod
+        method_name = 'get_%s' % (self.mod,)
+        self.get_data = getattr(self, method_name, self.__raise_no_module)
+
+    def get_cpu(self):
+        cmd = "sed -n 's/^cpu\s//p' /proc/stat"
+        process1 = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+        cpu_array1 = process1.communicate()[0].strip().split(' ')
+        total1 = 0
+        for val in cpu_array1:
+            total1 += int(val)
+
+        time.sleep(1)
+
+        process2 = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+        cpu_array2 = process2.communicate()[0].strip().split(' ')
+        total2 = 0
+        for val in cpu_array2:
+            total2 += int(val)
+
+        diff = total2 - total1
+        result = collections.OrderedDict()
+        key_list = (
+            'user', 'nice', 'system', 'idle', 'iowait', 'irq', 'softirq',
+            'steal',
+            'guest', 'guest_nice')
+
+        for i in range(10):
+            result[key_list[i]] = (float(cpu_array2[i]) - float(
+                cpu_array1[i])) / diff
+
+        return result
+
+    def get_memory(self):
+        cmd = "cat /proc/meminfo | grep -w 'MemTotal:\|MemFree:\|Buffers: \
+        \|Cached:\|Active:\|Inactive:\|SwapTotal:\|SwapFree:'"
+        process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+        list = []
+        for val in process.communicate()[0].strip().split('\n'):
+            list.append(int(val.split(' ')[-2]) * 1024)
+
+        result = collections.OrderedDict()
+        result['total'] = list[0]
+        result['used'] = list[0] - list[1]
+        result['abs_used'] = list[0] - (list[1] + list[2] + list[3])
+        result['free'] = list[1]
+        result['buffers'] = list[2]
+        result['cached'] = list[3]
+        result['active'] = list[4]
+        result['inactive'] = list[5]
+        result['swap_used'] = list[6] - list[7]
+
+        return result
+
+    def get_load(self):
+        cmd = "cat /proc/loadavg"
+        process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+        data = process.communicate()[0].strip().split(' ')
+        result = collections.OrderedDict()
+        key_list = ('w1_avg', 'w5_avg', 'w15_avg')
+
+        for i in range(3):
+            result[key_list[i]] = data[i]
+
+        return result
+
+    def __raise_no_module(self):
+        raise ModuleException('no such module ' + self.mod)
+
+    def catch(self):
+        return self.get_data()
