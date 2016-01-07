fe3ea5734f91cd8fdaaacbb1b38a1779601af6f2
zhouboyu <zhouboyu396@gmail.com>
2016-01-07 20:47:24 +0800

    add System-V style init script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,56 @@
-#/bin/sh
+#! /bin/sh
 
-cd ../code && python ../code/agent.py -t 5 -m cpu
+### BEGIN INIT INFO
+# Provides:          trainning
+# Required-Start:
+# Required-Stop:
+# Should-Start:
+# Should-Stop:
+# Default-Start:
+# Default-Stop:
+# Short-Description: Provide service to run trainning
+# Description: Control agent and other background service
+### END INIT INFO
+
+Module="$2"
+
+module_start() {
+  case "$1" in
+    agent)
+        python ../code/data/kafkaProducer.py
+        python ../code/data/kafkaConsumer.py
+        ;;
+    server)
+        python ../code/data/server.py
+        ;;
+    router)
+        python ../code/router.py
+        ;;
+    *)
+        echo $"Usage: $0 {start|stop|restart|reload|status} MODULE\n"
+        echo "MODULES:\n"
+        echo "agent,  server, router"
+        exit 1 
+  esac
+}
+
+case "$1" in
+  start)
+        module_start $Module
+        ;;
+  stop)
+        exit 0
+        ;;
+  status)
+        ;;
+  restart)
+        exit 0
+        ;;
+  reload)
+	exit 0
+        ;;
+  *)
+        echo $"Usage: $0 {start|stop|restart|reload|status} MODULE\n"
+        exit 1
+esac
+exit 0
